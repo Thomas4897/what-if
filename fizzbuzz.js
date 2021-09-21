54c3bf57afd57886a065d3bf8c92ba3cab83ca80
Thomas4897 <t.maynard4897@gmail.com>
2021-09-22 04:05:21 +0800

    Final Final

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,6 +4,7 @@ const num = Number(prompt("Please enter a number: "));
 const remainderThree = num % 3;
 const remainderFive = num % 5;
 
+// if (num % 15 === 0) {
 if (remainderThree === 0 && remainderFive === 0) {
 	console.log("fizzbuzz");
 } else if (remainderThree === 0) {
@@ -11,5 +12,27 @@ if (remainderThree === 0 && remainderFive === 0) {
 } else if (remainderFive === 0) {
 	console.log("buzz");
 } else {
-	console.log("Error Not Divisble by 3 or 5.");
+	console.log("Error: Not Divisble by 3 or 5.");
 }
+
+// if (num % 3 === 0 && num % 5 === 0) {
+// 	console.log("fizzbuzz");
+// } else if (num % 3 === 0) {
+// 	console.log("fizz");
+// } else if (num % 5 === 0) {
+// 	console.log("buzz");
+// } else {
+// 	console.log("Error: Not Divisble by 3 or 5.");
+// }
+
+// if (num % 3 === 0) {
+// 	if (num % 5 === 0) {
+// 		console.log("fizzbuzz");
+// 	} else {
+// 		console.log("fizz");
+// 	}
+// } else if (num % 5 === 0) {
+// 	console.log("buzz");
+// } else {
+// 	console.log("Error: Not Divisble by 3 or 5.");
+// }
